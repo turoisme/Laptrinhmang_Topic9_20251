8f5a5c35c68ed8473d29e4071500df90b4f29451
turoisme <turo@LAPTOP-LNQI9FRN.localdomain>
2025-12-23 19:59:26 +0700

    Update handle_room, handle_iten

--- Server/item.c
+++ Server/item.c
@@ -5,17 +5,88 @@
 #include <stdlib.h>
 #include "item.h"
 #include "protocol.h"
+#include "database.h"
+#include "socket_handler.h"
 
 int handle_create_item(char *message, int sockfd) {
-	return FUNCTION_IN_DEV;
-}
-
-int handle_delete_item(char *message, int sockfd) {
-	return FUNCTION_IN_DEV;
+	char item_name[100];
+	int start_price, buy_now_price;
+	
+	// Parse: CREATE_ITEM item_name start_price buy_now_price
+	if (sscanf(message, "CREATE_ITEM %99s %d %d", item_name, &start_price, &buy_now_price) != 3) {
+		return FORMAT_ERROR;
+	}
+	
+	// Validate prices
+	if (start_price <= 0 || buy_now_price <= 0 || buy_now_price <= start_price) {
+		return INVALID_INPUT_PARAMETER;
+	}
+	
+	// TODO: Get room_id and owner_id from session (sockfd mapping)
+	// For now, use dummy values
+	int room_id = 1;
+	int owner_id = 1;
+	
+	// Create item in database
+	int item_id = db_item_create(item_name, room_id, owner_id, 
+	                             (double)start_price, (double)buy_now_price);
+	
+	if (item_id > 0) {
+		// Item created successfully
+		return ITEM_CREATED;
+	} else {
+		// Database error
+		return DATABASE_ERROR;
+	}
 }
 
 int handle_list_items(int sockfd) {
-	return FUNCTION_IN_DEV;
+	// TODO: Get room_id from session (sockfd mapping)
+	// For now, use dummy room_id = 1
+	int room_id = 1;
+	
+	char *item_list = db_item_list_by_room(room_id);
+	
+	if (item_list) {
+		// Format response with code 700
+		char response[4096];
+		snprintf(response, sizeof(response), "700 %s", item_list);
+		send_message(sockfd, response);
+		free(item_list);
+		return LIST_ITEMS_SUCCESS;
+	} else {
+		return DATABASE_ERROR;
+	}
 }
-
+int handle_delete_item(char *message, int sockfd) {
+	char item_name[100];
+	int start_price, buy_now_price;
+	
+	// Parse: CREATE_ITEM item_name start_price buy_now_price
+	if (sscanf(message, "CREATE_ITEM %99s %d %d", item_name, &start_price, &buy_now_price) != 3) {
+		return FORMAT_ERROR;
+	}
+	
+	// Validate prices
+	if (start_price <= 0 || buy_now_price <= 0 || buy_now_price <= start_price) {
+		return INVALID_INPUT_PARAMETER;
+	}
+	
+	// TODO: Get room_id and owner_id from session (sockfd mapping)
+	// For now, use dummy values
+	int room_id = 1;
+	int owner_id = 1;
+	
+	// Create item in database
+	int item_id = db_item_create(item_name, room_id, owner_id, 
+	                             (double)start_price, (double)buy_now_price);
+	
+	if (item_id > 0) {
+		// Item created successfully
+		return ITEM_CREATED;
+	} else {
+		// Database error
+		return DATABASE_ERROR;
+	}
+}
 
--- Server/room.c
+++ Server/room.c
@@ -3,19 +3,44 @@
 #include <stdlib.h>
 #include "room.h"
 #include "protocol.h"
+#include "database.h"
+#include "socket_handler.h"
 
 int handle_create_room(char *message, int sockfd) {
-	return FUNCTION_IN_DEV;
-}
-
-int handle_join_room(char *message, int sockfd) {
-	return FUNCTION_IN_DEV;
+	char room_name[100];
+	if (sscanf(message, "CREATE_ROOM %99s", room_name) != 1) {
+		return FORMAT_ERROR;
+	}
+	// TODO: Get user_id from session (sockfd mapping)
+	// For now, use dummy user_id = 1
+	int owner_id = 1;
+	int room_id = db_room_create(room_name, owner_id, 0, NULL);
+	if (room_id > 0) {
+		return ROOM_CREATED;
+	} else if (room_id == -2) {
+		return ROOM_EXISTS;
+	} else {
+		return DATABASE_ERROR;
+	}
 }
 
 int handle_list_rooms(int sockfd) {
+	char *room_list = db_room_list_all();
+	
+	if (room_list) {
+		// Format response with code 500
+		char response[4096];
+		snprintf(response, sizeof(response), "500 %s", room_list);
+		send_message(sockfd, response);
+		free(room_list);
+		return LIST_ROOMS_SUCCESS;
+	} else {
+		return DATABASE_ERROR;
+	}
+}
+int handle_join_room(char *message, int sockfd) {
 	return FUNCTION_IN_DEV;
 }
-
 int handle_leave_room(char *message, int sockfd) {
 	return FUNCTION_IN_DEV;
 }
