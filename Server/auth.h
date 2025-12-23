4e06647277cabf4f8124de2a8277fee7f7ff85f3
Vinh Lê <lethanhvinh3011@gmail.com>
2025-12-23 18:15:22 +0700

    Fully implemented login

--- Server/auth.h
+++ Server/auth.h
@@ -3,6 +3,7 @@
 #ifndef AUTH_H
 #define AUTH_H
 
+void reset_server_auth();
 int is_verified_user(int sockfd,char *account);  //Call this to check if the user is logged in
 int handle_login(char *message, int sockfd);
 int handle_register(char *message, int sockfd);
